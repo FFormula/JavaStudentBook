a8c9337cddcfa9f558d5aee93fbaf0618b570bc5
Jevgenij <fformula@gmail.com>
2018-11-22 13:10:41 +0200

    Klase viena pazymi saugoti

--- src/main/java/data/Point.java
+++ src/main/java/data/Point.java
@@ -1,4 +1,35 @@
 package data;
 
 public class Point {
+    private String subject;
+    private String teacher;
+    private int point;
+
+    public Point(String subject, String teacher, int point) {
+        if (point < 1 || point > 10)
+            throw new IllegalArgumentException(
+                    "Point must be from 1 to 10, given: " + point);
+        this.subject = subject;
+        this.teacher = teacher;
+        this.point = point;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    @Override
+    public String toString() {
+        return "Dalykas: " + subject +
+             ", Destytojas: " + teacher +
+             ", Pazymis: " + point;
+    }
 }
