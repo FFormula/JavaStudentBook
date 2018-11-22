ec14ce09dbcd76cd3ed5f52b9f7f63e44caa0806
Jevgenij <fformula@gmail.com>
2018-11-22 13:11:07 +0200

    Klase saugoti visu pazimiu sarasa

--- src/main/java/data/Book.java
+++ src/main/java/data/Book.java
@@ -1,4 +1,28 @@
 package data;
 
+import java.util.ArrayList;
+
 public class Book {
+    ArrayList<Point> list;
+
+    public Book() {
+        list = new ArrayList<>();
+    }
+
+    public void addPoint(Point point) {
+        list.add(point);
+    }
+
+    public ArrayList<Point> getPoints() {
+        return list;
+    }
+
+    public double getAveragePoint() {
+        if (list.size() == 0)
+            return 0;
+        int sum = 0;
+        for (Point point : list)
+            sum += point.getPoint();
+        return (double)sum / list.size();
+    }
 }
